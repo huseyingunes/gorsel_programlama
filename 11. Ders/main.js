75c6b33d2fb58b27662229f56299afa6dfc8540d
huseyin gunes <huseyingunes@gmail.com>
2023-05-18 10:41:11 +0300

    13. hafta ara gonderim

--- 11. Ders/main.js
+++ 11. Ders/main.js
@@ -15,7 +15,7 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools()
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('giris.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
